d919c051d48429f1b17cf78e46d7aa927f6f3af7
tich0425 <2981342943@qq.com>
2025-06-30 22:18:48 +0800

    feat: integrated element plus component

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,22 @@
 import {createApp} from 'vue'
 import App from './App.vue'
-import './style.css';
 
-createApp(App).mount('#app')
+// 引入 element-plus 插件与样式
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+// 配置 element-plus 国际化
+// @ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+// 引入全局样式
+import '@/styles/index.scss'
+
+const app = createApp(App)
+
+app.use(ElementPlus, {
+    locale: zhCn,
+})
+
+// 挂载应用
+app.mount('#app')
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,33 @@
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv, normalizePath} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {imageProps} from "element-plus";
+import path from 'path'
+import {createSvgIconsPlugin} from "vite-plugin-svg-icons";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig(({ command, mode }) => {
+  let env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [
+        vue(),
+        createSvgIconsPlugin({
+          iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+          symbolId: 'icon-[dir]-[name]',
+        })
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    },
+    // 配置 scss 全局变量
+    css: {
+      preprocessorOptions: {
+        scss: {
+          javascriptEnabled: true,
+          additionalData: '@use "@/styles/variable.scss" as *;',
+        }
+      }
+    }
+  }
 })
